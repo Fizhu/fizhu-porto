e085f0933b128a46f4fcf3570bb543a92ec2775c
fizhu <hvyz.anbiya@gmail.com>
2019-09-07 14:56:57 +0700

    basing

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log('Server Running on Port' +PORT);
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.render('index.ejs');
+});
